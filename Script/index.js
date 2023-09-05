77255390b9464414c857bd67e39d5bc9a215c6c0
Alexis-Zurano <alexis.zurano@gmail.com>
2023-09-05 00:41:32 -0300

    home finished

--- Script/index.js
+++ Script/index.js
@@ -3,3 +3,7 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () => {
   navbar.classList.toggle('active');
 }
+
+window.onscroll = () => {
+  navbar.classList.remove('active')
+}
